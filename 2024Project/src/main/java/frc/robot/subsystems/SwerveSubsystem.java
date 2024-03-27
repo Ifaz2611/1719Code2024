a71525aad5e905076a6d5115f43d8a1918627d0c
IBHishere <jais24@parkschool.net>
2024-03-27 16:54:27 -0400

    with swerve comments

--- 2024Project/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ 2024Project/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -31,16 +31,19 @@ public class SwerveSubsystem extends SubsystemBase {
     private static CANSparkMax RIGHT_FRONT_DRIVE_SPEED_MOTOR;
     private static CANSparkMax RIGHT_BACK_DRIVE_SPEED_MOTOR;
 
+    //PIDsubsystems
     public SwerveDirectionPIDSubsystem m_leftFrontDirection;
     public SwerveDirectionPIDSubsystem m_rightFrontDirection;
     public SwerveDirectionPIDSubsystem m_leftBackDirection;
     public SwerveDirectionPIDSubsystem m_rightBackDirection;
 
+    //Locations of wheels. Not currently in use
     Translation2d m_frontLeftLocation = new Translation2d(0.3,0.3);
     Translation2d m_frontRightLocation = new Translation2d(0.3,-0.3);
     Translation2d m_backLeftLocation = new Translation2d(-0.3,0.3);
     Translation2d m_backRightLocation = new Translation2d(-0.3 ,-0.3);;
 
+    //kinematics group, not currently used
     SwerveDriveKinematics m_Kinematics = new SwerveDriveKinematics(m_frontLeftLocation,  m_backLeftLocation, m_frontRightLocation, m_backRightLocation);
 
 
@@ -50,7 +53,7 @@ public class SwerveSubsystem extends SubsystemBase {
     public static RelativeEncoder RIGHT_FRONT_DRIVE_DISTANCE_ENCODER;
     public static RelativeEncoder RIGHT_BACK_DRIVE_DISTANCE_ENCODER;
 
-
+    //Wheel groups for each wheel
     SwerveDriveWheel LEFT_FRONT_DRIVE_WHEEL;
     SwerveDriveWheel LEFT_BACK_DRIVE_WHEEL;
     SwerveDriveWheel RIGHT_FRONT_DRIVE_WHEEL;
@@ -58,8 +61,10 @@ public class SwerveSubsystem extends SubsystemBase {
 
     // Gyro
     public static Pigeon2 DRIVE_GYRO;
-    public SwerveDriveCoordinator SWERVE_DRIVE_COORDINATOR;
 
+    // The coordinator holds everything and is what you need to call to drive the robot
+    public SwerveDriveCoordinator SWERVE_DRIVE_COORDINATOR;
+//initiation of a swervesubsystem with 4 PIDsubsystems
     public SwerveSubsystem(SwerveDirectionPIDSubsystem m_leftFrontDirection,
             SwerveDirectionPIDSubsystem m_leftBackDirection, SwerveDirectionPIDSubsystem m_rightFrontDirection,
             SwerveDirectionPIDSubsystem m_rightBackDirection) {
@@ -72,6 +77,7 @@ public class SwerveSubsystem extends SubsystemBase {
         RIGHT_BACK_DRIVE_SPEED_MOTOR = new CANSparkMax(Constants.RIGHT_BACK_DRIVE_SPEED_MOTOR_PIN,
                 MotorType.kBrushless);
 
+         //PID       
         this.m_leftFrontDirection = m_leftFrontDirection;
         this.m_rightFrontDirection = m_rightFrontDirection;
         this.m_leftBackDirection = m_leftBackDirection;
@@ -101,6 +107,7 @@ public class SwerveSubsystem extends SubsystemBase {
 
 
     public class SwerveDriveCoordinator {
+        //contains 4 wheels
         SwerveDriveWheel leftFrontWheel;
         SwerveDriveWheel leftBackWheel;
         SwerveDriveWheel rightFrontWheel;
@@ -108,6 +115,7 @@ public class SwerveSubsystem extends SubsystemBase {
 
         public SwerveDriveCoordinator(SwerveDriveWheel leftFrontWheel, SwerveDriveWheel leftBackWheel,
                 SwerveDriveWheel rightFrontWheel, SwerveDriveWheel rightBackWheel) {
+                    //initiate wheels
             this.leftFrontWheel = leftFrontWheel;
             this.leftBackWheel = leftBackWheel;
             this.rightFrontWheel = rightFrontWheel;
@@ -117,29 +125,38 @@ public class SwerveSubsystem extends SubsystemBase {
 
 
 
-      
+      //this drives the robot using robot oriented drive by just calling the below drifTranslate but negating the Gyro
         public void robotOrientedDrift(double direction, double translatePower, double turnPower){
             drifTranslate(direction+DRIVE_GYRO.getAngle(), translatePower, turnPower);
         }
         public void drifTranslate(double direction, double translatePower, double turnPower){
-                      direction = direction - DRIVE_GYRO.getAngle(); //gyro on hootide hence +90 took out +90 +30 + 180
-
+                      direction = direction - DRIVE_GYRO.getAngle(); //field oriented by subtracting gyro
+//uses convertToPower to figure out what each wheel should be doing. rotatedirection is based ont he location of the wheel
                       SwerveModuleState leftFrontPosition = convertToPower(translatePower, 225, turnPower, direction);
                       SwerveModuleState leftBackPosition = convertToPower(translatePower, 135, turnPower, direction);
                       SwerveModuleState rightFrontPosition = convertToPower(translatePower, 315, turnPower, direction);
                       SwerveModuleState rightBackPosition = convertToPower(translatePower, 45, turnPower, direction);
+                      //gives the swerve states to the wheels.
           leftFrontWheel.SwerveSetWithState(leftFrontPosition);
           leftBackWheel.SwerveSetWithState(leftBackPosition);
           rightFrontWheel.SwerveSetWithState(rightFrontPosition);
           rightBackWheel.SwerveSetWithState(rightBackPosition);
     }
+
+    //Does the trigonomotry to add vectors for direction, power, and twist.
     public SwerveModuleState convertToPower(double translatepower, double rotateDirection, double turnpower, double direction ){
+        //angle needed to turn minus the direction going is how much of the turning happens on that wheel
         double turnangle = rotateDirection-direction;
+        //these 2 convert the angle to an y and x coordinate on the unit circle
         double forwardCoeficient = Math.cos(Math.toRadians(turnangle))*turnpower;
         double leftCoeficient = Math.sin(Math.toRadians(turnangle))*turnpower;
+        //makes the robot go forward equal to the given power
         forwardCoeficient+=translatepower;
+        //the power from the wheel is based on the hypotonuse of forward and left.
         translatepower = Math.sqrt(forwardCoeficient*forwardCoeficient+leftCoeficient*leftCoeficient);
+        //the new rotation angle off of direction is based on the new left and forward coefficients
         Rotation2d turnangleRot = new Rotation2d(Math.atan2(leftCoeficient, forwardCoeficient)+ Math.toRadians(direction));
+        //gives back the state to set the swerve to
 SwerveModuleState Moduleset = new SwerveModuleState( translatepower, turnangleRot);
 
 return Moduleset;
@@ -174,7 +191,7 @@ return Moduleset;
         RIGHT_BACK_DRIVE_DISTANCE_ENCODER.setPosition(0);
     }
     
-
+   // gets the average distance the motors have driven so far
     public double returnAverageDistance() {
         return (LEFT_FRONT_DRIVE_DISTANCE_ENCODER.getPosition() +
          LEFT_BACK_DRIVE_DISTANCE_ENCODER.getPosition() + 
